aab2ed219fe39a483a8d9a3e959116d591f6a4aa
Daniel Alejandro Fernandez <daf111@yahoo.com.ar>
2023-03-24 15:55:08 -0300

    Utilizar Feign client en lugar de RestTemplate

--- springboot-servicio-item/src/main/java/com/formacionbdi/springboot/app/item/SpringbootServicioItemApplication.java
+++ springboot-servicio-item/src/main/java/com/formacionbdi/springboot/app/item/SpringbootServicioItemApplication.java
@@ -1,9 +1,14 @@
 package com.formacionbdi.springboot.app.item;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.FeignAutoConfiguration;
 
 @SpringBootApplication
+@EnableFeignClients
+@ImportAutoConfiguration({FeignAutoConfiguration.class})
 public class SpringbootServicioItemApplication {
 
 	public static void main(String[] args) {
